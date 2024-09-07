ea8824cd1b50927f225b168df69c22ce9673ab60
Samson Odiagbe <sammiodiagbe@gmail.com>
2024-09-06 17:23:51 -0700

    ui: adjusting screen size on large screen

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Link from "next/link";
 export default async function Index() {
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main className="flex-1 flex flex-col gap-6 px-4 lg:container">
         <Navbar />
         <div className="flex px-4 py-4 ">
           <section className="w-[35rem] space-y-8">
@@ -42,7 +42,7 @@ export default async function Index() {
               height={500}
               width={500}
               alt="Car sideways"
-              className="w-auto h-auto"
+              className="w-full h-auto"
             />
           </section>
         </div>
@@ -58,14 +58,14 @@ export default async function Index() {
           <WhyChooseUsComponent />
         </section>
         <Testimonials />
-        <section className="flex p-4 items-center">
+        <section className="flex p-4 items-center gap-[30px]">
           <div className="flex-1">
             <Image
               src={"/images/hero_image2.svg"}
               height={500}
               width={500}
               alt="Car sideways"
-              className="w-auto h-auto"
+              className="w-full h-auto"
             />
           </div>
           <div className=" w-[30rem] space-y-5">
