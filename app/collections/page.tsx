8f23d9965ed4083662e6ba55aa47359458af340c
Samson Odiagbe <sammiodiagbe@gmail.com>
2024-09-07 15:20:16 -0700

    ui: fixing up styles

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -2,7 +2,7 @@
 import CustomButton from "@/components/app-components/button";
 import { createClient } from "@/utils/supabase/client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 const Collections = () => {
   const supabase = createClient();
@@ -34,10 +34,10 @@ const Collections = () => {
       <h1 className="my-5 text-2xl font-bold text-gray-700">
         Our collections.
       </h1>
-      <section className="grid grid-cols-3 gap-[50px]">
+      <section className="grid grid-cols-3 gap-[30px]">
         {allCars?.map((car, index) => {
           return (
-            <div className="flex  flex-col justify-between" key={index}>
+            <div className="flex  flex-col" key={index}>
               <Image
                 src={car.image_url}
                 alt={car.name}
@@ -53,12 +53,41 @@ const Collections = () => {
                     <span className="ml-1 text-xs">per day</span>
                   </span>
                 </div>
-
-                <div className="flex justify-end">
-                  <CustomButton
-                    title="Book now"
-                    style="bg-green-500 hover:bg-green-600"
+                <div className="flex items-center gap-1">
+                  <Image
+                    src={"/icons/Gauge.svg"}
+                    height={24}
+                    width={24}
+                    className="h-5 w-5"
+                    alt="allowd km per day"
                   />
+                  <span className="text-sm">{car.allowed_km} KM per day</span>
+                </div>
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-1">
+                    <FeatureComponent
+                      img_path="/icons/Seat.svg"
+                      value={car.doors}
+                    />
+                    <FeatureComponent
+                      img_path="/icons/Users.svg"
+                      value={car.seats}
+                    />
+                    <FeatureComponent
+                      img_path="/icons/Engine.svg"
+                      value={"Automatic"}
+                    />
+                  </div>
+                  <div className="flex justify-end gap-2">
+                    <CustomButton
+                      title="Reserve"
+                      style="bg-gray-200 hover:bg-gray-300 text-black"
+                    />
+                    <CustomButton
+                      title="Book now"
+                      style="bg-green-500 hover:bg-green-600"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
@@ -66,6 +95,26 @@ const Collections = () => {
         })}
       </section>
     </main>
+  );
+};
+
+type TFeature = {
+  img_path: string;
+  value: string;
+};
+
+const FeatureComponent: FC<TFeature> = ({ img_path, value }) => {
+  return (
+    <span className="flex gap-1 items-center">
+      <Image
+        src={img_path}
+        alt={value}
+        height={24}
+        width={24}
+        className="w-5 h-5"
+      />
+      <span className="text-xs">{value}</span>
+    </span>
   );
 };
 
