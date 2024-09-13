9d84c1aedad5c429098a3202b106b95e7d41f9fa
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-09-13 07:26:17 -0700

    implementing logging with google

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,11 +1,24 @@
+"use client";
 import { signInAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 
 export default function Login({ searchParams }: { searchParams: Message }) {
+  const supabase = createClient();
+
+  const attemptLoginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000",
+      },
+    });
+  };
   return (
     <form className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
@@ -33,6 +46,11 @@ export default function Login({ searchParams }: { searchParams: Message }) {
           placeholder="Your password"
           required
         />
+        <div className="">
+          <Button className="w-full" onClick={attemptLoginWithGoogle}>
+            Sign in with Google
+          </Button>
+        </div>
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
           Sign in
         </SubmitButton>
