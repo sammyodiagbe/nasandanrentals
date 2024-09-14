a4d3376ad3621b129dcb9f6b8a5b70cbcd5fdf82
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-09-14 16:16:43 -0700

    fixing deployment bugs

--- app/payment-successful/page.tsx
+++ app/payment-successful/page.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Suspense } from "react";
 
 const PaymentSuccessful = () => {
   const search = useSearchParams();
@@ -11,10 +12,12 @@ const PaymentSuccessful = () => {
     <main className="py-8">
       <div className="max-w-[550px] space-y-2 p-4 shadow-sm mx-auto">
         <h1 className="text-5xl  font-bold">Payment successful</h1>
-        <p>
-          Thank you for booking <b>{carName}</b> with us, pleae make sure to
-          bring your governent issued ID when coming for pickup.
-        </p>
+        <Suspense>
+          <p>
+            Thank you for booking <b>{carName}</b> with us, pleae make sure to
+            bring your governent issued ID when coming for pickup.
+          </p>
+        </Suspense>
         <div className="flex justify-end items-center gap-1 pt-8">
           <Link href="/bookings">
             <Button variant={"link"}> View your bookings</Button>
