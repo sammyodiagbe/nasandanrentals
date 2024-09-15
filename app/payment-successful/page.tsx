f1dabcfb56aeda7ed64fec351abf7b460f5d980b
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-09-15 10:23:45 -0700

    fixing bugs

--- app/payment-successful/page.tsx
+++ app/payment-successful/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Suspense } from "react";
