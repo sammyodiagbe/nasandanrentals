0092cc59269bced926735b24f5ff7d550d0d047c
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-09-15 10:27:09 -0700

    fixing bugs

--- app/payment-successful/page.tsx
+++ app/payment-successful/page.tsx
@@ -8,20 +8,22 @@ import ParagraphSuspense from "@/components/suspense/paragraph";
 const PaymentSuccessful = () => {
   return (
     <main className="py-8">
-      <div className="max-w-[550px] space-y-2 p-4 shadow-sm mx-auto">
-        <h1 className="text-5xl  font-bold">Payment successful</h1>
+      <Suspense>
+        <div className="max-w-[550px] space-y-2 p-4 shadow-sm mx-auto">
+          <h1 className="text-5xl  font-bold">Payment successful</h1>
 
-        <ParagraphSuspense />
+          <ParagraphSuspense />
 
-        <div className="flex justify-end items-center gap-1 pt-8">
-          <Link href="/bookings">
-            <Button variant={"link"}> View your bookings</Button>
-          </Link>
-          <Link href={"/collections"}>
-            <Button variant={"link"}>View Collections</Button>
-          </Link>
+          <div className="flex justify-end items-center gap-1 pt-8">
+            <Link href="/bookings">
+              <Button variant={"link"}> View your bookings</Button>
+            </Link>
+            <Link href={"/collections"}>
+              <Button variant={"link"}>View Collections</Button>
+            </Link>
+          </div>
         </div>
-      </div>
+      </Suspense>
     </main>
   );
 };
