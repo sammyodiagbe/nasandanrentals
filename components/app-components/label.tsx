4b86ddc9707f5de5d4ff09252fdd8bf646a7ce6b
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-09-27 13:25:45 -0400

    fixing bugs

--- components/app-components/label.tsx
+++ components/app-components/label.tsx
@@ -5,11 +5,7 @@ type TLabelComponent = {
   value: string;
 };
 const LabelComponent: FC<TLabelComponent> = ({ htmlFor, value }) => {
-  return (
-    <label htmlFor={htmlFor} className="text-sm block mb-1">
-      {value}
-    </label>
-  );
+  return <label className="text-sm block mb-1">{value}</label>;
 };
 
 export default LabelComponent;
