562662511974e9d8f859813437c46a12c6fe3f52
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-10-02 19:18:21 -0400

    fixing pricing bugs

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from "stripe";
 export const stripeClient = new Stripe(process.env.STRIPE_API_KEY!);
 
 export const calculateStripeFees = (price: number) => {
-  const cal = price + price * 0.029 + 0.3;
+  const cal = price / (1 - 0.029) + 0.3;
   return cal * 100;
 };
