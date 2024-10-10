6122020d4d77b60d2e24a5ca25b95c2a8b2fd148
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-10-10 14:54:45 -0400

    creating custom hooks and cleaning up my code

--- app/rent/page.tsx
+++ app/rent/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import { createClient } from "@/utils/supabase/client";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
-import { z } from "zod";
-import { SubmitHandler, useForm, Controller, useWatch } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Suspense } from "react";
+import { Controller } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
@@ -25,118 +21,15 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { generateTime } from "@/utils/timeOptions";
-import { bookVehicle } from "../actions";
 import FormErrorMessage from "@/components/formErrorMessage";
 import LabelComponent from "@/components/app-components/label";
-import { getNumberOfDays, isPastDate } from "@/utils/dateFormat";
-import { useToast } from "@/hooks/use-toast";
-
-const formSchema = z.object({
-  pickupDate: z.date(),
-  pickupTime: z.string(),
-  returnDate: z.date(),
-  returnTime: z.string(),
-  fullname: z.string(),
-  emailAddress: z.string(),
-  address: z.string(),
-  phonenumber: z.string().regex(new RegExp(/^\d{10}$/)),
-});
-
-type FormSchema = z.infer<typeof formSchema>;
+import useRentHook from "@/custom-hooks/useRentHook";
 
 const times = generateTime(7, 17);
 
 const RentPage = () => {
-  const searchParams = useSearchParams();
-  const supabase = createClient();
-  const carId = searchParams.get("car_id");
-  const [car, setCar] = useState<any | null>(null);
-  const { toast } = useToast();
-  const {
-    handleSubmit,
-    control,
-
-    formState: { errors, isSubmitting },
-  } = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      pickupTime: "7:00",
-      returnTime: "7:00",
-    },
-  });
-
-  const [pickupDate, returnDate, returnTime, pickupTime] = useWatch({
-    control,
-    name: ["pickupDate", "returnDate", "returnTime", "pickupTime"],
-    defaultValue: {
-      pickupDate: new Date(Date.now()),
-      returnDate: new Date(Date.now()),
-      pickupTime: "7:00",
-      returnTime: "7:00",
-      phonenumber: "",
-    },
-  });
-
-  useEffect(() => {
-    getCarData();
-  }, [carId]);
-
-  const getCarData = async () => {
-    const { data: car, error } = await supabase
-      .from("cars")
-      .select()
-      .eq("id", carId);
-    if (error) {
-      return;
-    }
-    if (car) {
-      setCar(car[0]);
-      // setError(false);
-    }
-  };
-
-  const book: SubmitHandler<FormSchema> = async (formData) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    const { pickupDate, pickupTime, returnDate, returnTime } = formData;
-    const numberOfDays: number = getNumberOfDays(
-      pickupDate,
-      pickupTime,
-      returnDate,
-      returnTime
-    );
-
-    const pastDate =
-      isPastDate(pickupDate, pickupTime) || isPastDate(returnDate, returnTime);
-
-    if (pastDate) {
-      toast({
-        title: "Date is in past",
-        description: "One of the date is in the past please",
-      });
-      return;
-    }
-
-    if (numberOfDays < 1) {
-      toast({
-        title: "Invalid date selected",
-        description:
-          "You have selected an invalid date range, please make another selection please",
-      });
-
-      return;
-    }
-    await bookVehicle({
-      ...formData,
-      carId: carId!,
-      totalCost: car.price * numberOfDays,
-    });
-  };
-
-  let days = getNumberOfDays(pickupDate, pickupTime, returnDate, returnTime);
-  const bookAction = (formData: FormData) => {
-    console.log(formData);
-  };
+  const { car, control, handleSubmit, book, errors, days, isSubmitting } =
+    useRentHook();
   return (
     <main className="lg:container py-4">
       <div className="p-4">
