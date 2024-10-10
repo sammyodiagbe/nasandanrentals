04ab072fda304b09b5670411b32cf62af885dca5
Samson Odiagbe <odiagbesamsonosaro@gmail.com>
2024-10-10 11:49:12 -0400

    allowing no authenticated users to book vehicles

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -50,16 +50,16 @@ export const updateSession = async (request: NextRequest) => {
       return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
-    if (request.nextUrl.pathname.startsWith("/rent") && user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+    // if (request.nextUrl.pathname.startsWith("/rent") && user.error) {
+    //   return NextResponse.redirect(new URL("/sign-in", request.url));
+    // }
 
-    if (
-      request.nextUrl.pathname.startsWith("/booking-confirmed") &&
-      user.error
-    ) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/booking-confirmed") &&
+    //   user.error
+    // ) {
+    //   return NextResponse.redirect(new URL("/sign-in", request.url));
+    // }
     // if (request.nextUrl.pathname === "/" && !user.error) {
     //   return NextResponse.redirect(new URL("/", request.url));
     // }
